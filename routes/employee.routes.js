88764eb953eb7eabb1ec2f1b58b6d6e0574a9635
Fhatuwani Raphalalani <fm.raphalalani29@gmail.com>
2023-01-19 15:26:37 +0200

    edited routes

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -5,7 +5,7 @@ module.exports = app => {
     router.post("/", employee.create)
     router.get("/", employee.findAll)
     router.get("/:id", employee.findOne)
-    router.put("/:id", employee.update)
+    router.patch("/:id", employee.update)
     router.delete("/:id", employee.delete)
     router.delete("/", employee.deleteAll)
 
